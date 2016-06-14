4f3fa56c98536bf06118fd3cef020c4a8c0338fe
yinyong02 <yinyong02@baidu.com>
2016-06-14 16:06:00 +0800

    stub complete

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+  * Copyright (C) 2016 yanni4night.com
+  * index.js
+  *
+  * changelog
+  * 2016-06-14[16:03:54]:revised
+  *
+  * @author yanni4night@gmail.com
+  * @version 1.0.0
+  * @since 1.0.0
+  */
